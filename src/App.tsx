f59e287e0e47a924919906416ae619969c105d92
TylerGwynn <tylergwynn23@gmail.com>
2020-03-03 00:54:35 -0500

    added basic user component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserComponent from './UserComponent';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        {/* <a href="/userPage.html">User Page</a> */}
+        <UserComponent/>
       </header>
     </div>
   );
