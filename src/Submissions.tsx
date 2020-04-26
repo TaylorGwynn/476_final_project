ea8331f953f9afe4992dd77b06564b6732e0ffa1
TylerGwynn <tylergwynn23@gmail.com>
2020-04-25 21:54:34 -0400

    Added less goofy background, started "new poll"

--- src/Submissions.tsx
+++ src/Submissions.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Submissions extends Component<any, any> {
     private readonly inputOpenFileRef: React.RefObject<HTMLInputElement> = React.createRef();
-    state = { image: undefined, entryName: "", error: "", }
+    state = { image: undefined, entryName: "", pollEntryName: "", error: "", submissionMsg: "", pollSubmissionMsg: ""}
     constructor(props: any, state: any) {
         super(props)
         this.handleClick.bind(this);
@@ -28,10 +28,25 @@ export default class Submissions extends Component<any, any> {
         })
         if (this.state.entryName.length > 0) this.setState({ error: "" })
     }
+    setPollEntryName = (e:any) => {
+        e.persist();
+        this.setState({
+            pollEntryName : e.target.value
+        })
+        if (this.state.pollEntryName.length > 0) this.setState({ error: "" })
+    }
 
     renderError() {
         const { entryName } = this.state
         return <Form.Label>{this.state.error}</Form.Label>
+    }
+    renderSubmissionMsg(){
+        const { entryName } = this.state
+        return <Form.Label>{this.state.submissionMsg}</Form.Label>
+    }
+    renderPollSubmissionMsg(){
+        const { pollEntryName } = this.state    
+        return <Form.Label>{this.state.pollSubmissionMsg}</Form.Label>
     }
 
     submitEntry = () => {
@@ -45,15 +60,30 @@ export default class Submissions extends Component<any, any> {
         if (!result) {
             this.setState({ error: "This entry already exists in the poll!" })
         } else {
-            this.setState({ error: "Successfully added your entry." })
+            this.setState({ submissionMsg: "Successfully added your entry." })
         }
+    }
+    submitPollEntry = () => {
+        if (this.state.pollEntryName === "") {
+            this.setState({
+                error: "Please enter a name for your Poll entry."
+            })
+            return
+        }
+        //TODO make this into a new poll rather than a new entry!
+        // let result = this.props.newEntry({option: this.state.pollEntryName, image: this.state.image})
+        // if (!result) {
+        //     this.setState({ error: "This Poll already exists!" })
+        // } else {
+        //     this.setState({ pollSubmissionMsg: "Successfully added your Poll entry?" })
+        // }
     }
 
     render() {
         return (
             <div id="Submissions">
                 <div className="container">
-                    <div id="entry">
+                    <div id="optionEntry">
                         <div id="imageField">
                             <img id="profile-picture" 
                                 src={
@@ -78,10 +108,26 @@ export default class Submissions extends Component<any, any> {
                             <Form.Group>
                                 <Form.Label>Waifu Name</Form.Label>
                                 <Form.Control type="text" placeholder="Waifu Name" onChange={this.setEntryName}/>
-                                {this.renderError()}
+                                {this.renderSubmissionMsg()}
                                 <Form.Text className="text-muted">Enter your waifu's name!</Form.Text>
                             </Form.Group>
                             <Button variant="primary" onClick={this.submitEntry}>Submit</Button>
+                            </Form>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div id="pollEntry">
+                        <div id="pollNameField">
+                            <h1>Submit your new poll!</h1>
+                            <Form onSubmit={(e : any) => {e.preventDefault()}}>
+                            <Form.Group>
+                                <Form.Label>Poll Title</Form.Label>
+                                <Form.Control type="text" placeholder="Poll Name" onChange={this.setPollEntryName}/>
+                                {this.renderPollSubmissionMsg()}
+                                {/* <Form.Text className="text-muted">Enter your waifu's name!</Form.Text> */}
+                            </Form.Group>
+                            <Button variant="primary" onClick={this.submitPollEntry}>Submit</Button>
                             </Form>
                         </div>
                     </div>
